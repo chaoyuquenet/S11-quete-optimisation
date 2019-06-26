34619d5ad0587dfbe2b8938e5f8b0863a37458ca
chaoyuquenet <chaoyu1979@gmail.com>
2019-06-26 10:13:50 +0200

    optimiser le performance

---
+++ web-app/build/precache-manifest.8f611ee6710e83bef76ef675ade8e50a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb5f999b24fc71554917d1f62d51d97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39294fa9cd6ef947e5ad",
+    "url": "/static/js/2.f0b0e0d5.chunk.js"
+  },
+  {
+    "revision": "1d776562f41fceae12eb",
+    "url": "/static/js/main.7598485f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
